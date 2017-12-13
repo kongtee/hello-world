bfa2f137816da267b943a0d927f604899b9e655c
kongtee <kongtee@foxmail.com>
2017-12-13 23:50:14 +0800

    ADD：添加首页数据。

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,50 +5,8 @@ const app = getApp()
 Page({
   data: {
     headerBg: 'http://r.ezenlive.cn/ksyunadmin/5a1e8456ef3c733349.jpeg',
-    imgUrls: [
-      'http://r.ezenlive.cn/ksyunadmin/5a2106a7619be23043.jpeg',
-      'http://r.ezenlive.cn/ksyunadmin/5a21172337f1d91928.jpeg',
-      'http://r.ezenlive.cn/ksyunadmin/5a2106f04db5890130.jpeg',
-      'http://r.ezenlive.cn/ksyunadmin/5a1e86168692695029.jpeg',
-      'http://r.ezenlive.cn/ksyunadmin/5a1e85f50937b91227.jpeg'
-    ],
-    piclist: [
-      {
-        RoomId: 10001,
-        LiveId: 10001,
-        AnchorUrl: 'http://r.ezenlive.cn/ksyunadmin/5a1e9d94ddbe552972.jpeg',
-        Thumbnail: 'http://r.ezenlive.cn/ksyunadmin/5a1e9d94ddbe552972.jpeg',
-        AnchorDec: '丰满可爱的美女小可爱'
-      },
-      {
-        RoomId: 10002,
-        LiveId: 10002,
-        AnchorUrl: 'http://r.ezenlive.cn/ksyunadmin/5a1e85c7e128025793.jpeg',
-        Thumbnail: 'http://r.ezenlive.cn/ksyunadmin/5a1e85c7e128025793.jpeg',
-        AnchorDec: '可爱大胸美女私拍套图'
-      },
-      {
-        RoomId: 10003,
-        LiveId: 10003,
-        AnchorUrl: 'http://r.ezenlive.cn/ksyunadmin/5a1e85b84ad8c33290.jpeg',
-        Thumbnail: 'http://r.ezenlive.cn/ksyunadmin/5a1e85b84ad8c33290.jpeg',
-        AnchorDec: '可爱大尺度勾魂照片'
-      },
-      {
-        RoomId: 10004,
-        LiveId: 10004,
-        AnchorUrl: 'http://r.ezenlive.cn/ksyunadmin/5a1e85903345e21877.jpeg',
-        Thumbnail: 'http://r.ezenlive.cn/ksyunadmin/5a1e85903345e21877.jpeg',
-        AnchorDec: '可爱大胸美女私拍套图'
-      },
-      {
-        RoomId: 10005,
-        LiveId: 10005,
-        AnchorUrl: 'http://r.ezenlive.cn/ksyunadmin/5a1e85807b51f35184.jpeg',
-        Thumbnail: 'http://r.ezenlive.cn/ksyunadmin/5a1e85807b51f35184.jpeg',
-        AnchorDec: '可爱性感美女私拍套图'
-      }
-    ],
+    BannerList: [],
+    RecommendImageList: [],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -65,6 +23,26 @@ Page({
     })
   },
   onLoad: function () {
+    wx.request({
+      url: 'http://www.daybreakhouse.cn/v2.0/recommend/queryrecommendlist', 
+      data: {
+        QueryStartPos: 0,
+        QueryNumber: 10
+      },
+      method: 'POST',
+      success: (res) => {
+        console.log(res.data);
+        let resData = res.data || {};
+        if (resData.RspHeader && resData.RspHeader.ErrNo == 200) {
+          let rspJson = resData.RspJson || [];
+          this.setData({
+            BannerList: rspJson.BannerList,
+            RecommendImageList: rspJson.RecommendImageList
+          });
+        }
+      }
+    })
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
