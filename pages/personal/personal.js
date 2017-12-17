54d6861d7cd3fd06c0cb430463058757e5f164f9
kongtee <kongtee@foxmail.com>
2017-12-17 12:17:05 +0800

    ADD:添加个人信息页。

---
+++ pages/personal/personal.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const recommend = require('../../common/request/recommend')
+
+Page({
+  data: {
+    headerBg: 'http://r.ezenlive.cn/ksyunadmin/5a1e8456ef3c733349.jpeg',
+    BannerList: [],
+    RecommendImageList: [],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  enterAlbum: function(e) {
+    let url = '../detail/detail?id=' + e.currentTarget.dataset.id + '&title=' + e.currentTarget.dataset.title;
+    wx.navigateTo({
+      url: url
+    })
+  },
+  onLoad: function () {
+    wx.request({
+      url: recommend.queryrecommendlist, 
+      data: {
+        QueryStartPos: 0,
+        QueryNumber: 10
+      },
+      method: 'POST',
+      success: (res) => {
+        console.log(res.data);
+        let resData = res.data || {};
+        if (resData.RspHeader && resData.RspHeader.ErrNo == 200) {
+          let rspJson = resData.RspJson || [];
+          this.setData({
+            BannerList: rspJson.BannerList,
+            RecommendImageList: rspJson.RecommendImageList
+          });
+        }
+      }
+    })
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
